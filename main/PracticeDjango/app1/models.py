654ba1d139cd8e0ef5b417ff7c2daaa2e4d37a87
Kiran Deep <kirandeep102030@gmail.com>
2022-03-15 14:33:26 +0530

    Added models

--- main/PracticeDjango/app1/models.py
+++ main/PracticeDjango/app1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    likes = models.IntegerField(default=0)
+    postedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
