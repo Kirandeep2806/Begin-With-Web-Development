792a8e48375b61ad056d02bd7098dc61db9a7084
Kiran Deep <kirandeep102030@gmail.com>
2022-04-20 21:20:35 +0530

    Added multiple styles in single stmt(JS)

--- Lab/Week-4 Assignment/multiplicationTable.js
+++ Lab/Week-4 Assignment/multiplicationTable.js
@@ -6,11 +6,18 @@ const getTable = document.getElementById('mul-table');
 if(!rows || !cols)
     rows = cols = 10;
 
+const myStyle = `
+    padding: 10px;
+    text-align: center;
+`;
+
 for(let i=1; i<=rows; i++) {
     let createElement1 = document.createElement('tr');
     for(let j=1; j<=cols; j++) {
         const createElement2 = document.createElement('td');
         createElement2.innerHTML = i*j;
+        createElement2.style = myStyle;
+        console.log(createElement2.style);
         createElement1.append(createElement2);
     }
     getTable.append(createElement1);
