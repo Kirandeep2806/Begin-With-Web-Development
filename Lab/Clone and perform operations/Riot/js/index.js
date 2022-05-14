716fa2b347e09e8bc1a005561757d83cf7a566c1
Kiran Deep <kirandeep102030@gmail.com>
2022-05-14 18:08:32 +0530

    Background image is set for 1st section

---
+++ Lab/Clone and perform operations/Riot/js/index.js
@@ -0,0 +1,5 @@
+const setBgImageStart = document.getElementsByClassName('section-container__1')[0];
+const indexImage1 = new Image();
+indexImage1.src = '../static/annie-tibber-statue-day.png';
+
+setBgImageStart.style.height = `${indexImage1.height}px`;
