1186e07055fbda996b22e4562ecd05ea8d27703a
Kiran Deep <kirandeep102030@gmail.com>
2022-05-15 10:42:40 +0530

    Background is set👍

--- Lab/Clone and perform operations/Riot/js/index.js
+++ Lab/Clone and perform operations/Riot/js/index.js
@@ -1,5 +1,15 @@
-const setBgImageStart = document.getElementsByClassName('section-container__1')[0];
-const indexImage1 = new Image();
-indexImage1.src = '../static/annie-tibber-statue-day.png';
+// const setBgImageStart = document.getElementsByClassName('section-container__1')[0];
+// const indexImage1 = new Image();
+// setBgImageStart.style.backgroundImage = `url("../static/annie-tibber-statue-day.png")`;
+// setBgImageStart.style.backgroundSize = '100% 100%';
+// setBgImageStart.style.backgroundRepeat = 'no-repeat';
+// setBgImageStart.style.backgroundPosition = 'top center';
+// indexImage1.src = '../static/annie-tibber-statue-day.png';
+// setBgImageStart.style.height = `${indexImage1.height}px`;
+// console.log(setBgImageStart.clientHeight);
 
-setBgImageStart.style.height = `${indexImage1.height}px`;
+// window.addEventListener('resize', () => {
+//     console.log(setBgImageStart.style.backgroundSize);
+//     // console.log(setBgImageStart.getBoundingClientRect);
+// })
+
