587d827911e41467f47710813918489a43d3d447
Kiran Deep <kirandeep102030@gmail.com>
2022-05-17 20:29:29 +0530

    My-Part is 👍(Waiting for contributors)

--- Lab/Clone and perform operations/Riot/js/index.js
+++ Lab/Clone and perform operations/Riot/js/index.js
@@ -2,8 +2,3 @@ const getFooterToGoToTop = document.getElementsByClassName('go-up')[0];
 getFooterToGoToTop.addEventListener('click', () => {
     window.scrollTo('0', '0');
 })
-
-// const footerLogo1 = document.querySelector('.footer-logo-1');
-// footerLogo1.addEventListener('click', () => {
-//     location.href('https://www.facebook.com/');
-// })
