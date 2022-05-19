26f4dd52160adad55c0c30a1531ba9ea8fec815d
Kiran Deep <kirandeep102030@gmail.com>
2022-05-19 17:59:05 +0530

    mouse down change background staging

--- Lab/Clone and perform operations/Riot/js/index.js
+++ Lab/Clone and perform operations/Riot/js/index.js
@@ -23,4 +23,22 @@ document.addEventListener('mousedown', e => {
     let res = e.path.filter(e => regexpForCards1.test(e.className) || regexpForCards2.test(e.className));
     let getParent = res.pop();
     console.log(getParent);
-})
+    const changeColorOfCardContent = document.querySelector('.' + getParent.className + '__content');
+    const changeColorOfCardSlant = document.querySelector('.' + getParent.className + '__makeSlant');
+    changeColorOfCardContent.style.background = '#000';
+    changeColorOfCardSlant.style.background = '#000';
+    changeColorOfCardSlant.style.borderLeft = '#000';
+});
+
+document.addEventListener('mouseup', e => {
+    const regexpForCards1 = /section-container__2__col-4-cards-[1234]/;
+    const regexpForCards2 = /section-container__2__span_2-5/;
+
+    let res = e.path.filter(e => regexpForCards1.test(e.className) || regexpForCards2.test(e.className));
+    let getParent = res.pop();
+    console.log(getParent);
+    const changeColorOfCardContent = document.querySelector('.' + getParent.className + '__content');
+    const changeColorOfCardSlant = document.querySelector('.' + getParent.className + '__makeSlant');
+    changeColorOfCardContent.style.background = '#2b2a29';
+    changeColorOfCardSlant.style.background = '#2b2a29';
+});
