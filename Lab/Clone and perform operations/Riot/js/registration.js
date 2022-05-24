c5b2f242625deae7d401e59ff7963eb55d7dee2e
Kiran Deep <kirandeep102030@gmail.com>
2022-05-24 17:49:31 +0530

    Registration Form Staging#12

--- Lab/Clone and perform operations/Riot/js/registration.js
+++ Lab/Clone and perform operations/Riot/js/registration.js
@@ -5,5 +5,23 @@ getDob.addEventListener('change', () => {
     const dob = new Date(getDob.value);
     const ageDifMs = Date.now() - dob.getTime();
     const ageDate = new Date(ageDifMs);
-    getAge.value = Math.abs(ageDate.getUTCFullYear() - 1970);
+    let age = ageDate.getUTCFullYear() - 1970;
+    getAge.value = age > 0 ? age : 0;
 })
+
+const getForm = document.querySelector('.registration-card');
+getForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validation();
+});
+
+// User Name Validation - At least 6 characters, no special characters
+// Age - At least 16 years old
+// Password - At least 8 characters, no special characters, at least one number, one uppercase and one lowercase letter
+
+const validation = () => {
+    const uName = document.userDetails.uname.value;
+    const age = document.userDetails.age.value;
+    const password1 = document.userDetails.password.value;
+    const password2 = document.userDetails.confirmPassword.value;
+}
