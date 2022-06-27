df5af1e4eaf41758f31d2b7024922af22fd12be7
Kiran Deep <kirandeep102030@gmail.com>
2022-06-27 21:33:41 +0530

    Demonstration of Forms and Models Forms

--- Lab/django_forms/django_forms_app/forms.py
+++ Lab/django_forms/django_forms_app/forms.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from django import forms
 from .models import Student
 
@@ -17,3 +18,20 @@ class StudentForm(forms.Form):
         if age < 18:
             raise forms.ValidationError('This is not a valid age')
         return age  # return age if age is greater than 18
+
+
+class StudentModelForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+    
+    def clean_name(self):
+        if self.cleaned_data['name'] == 'admin':
+            raise forms.ValidationError('This is not a valid name')
+        return self.cleaned_data['name']
+    
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age < 18:
+            raise forms.ValidationError('This is not a valid age')
+        return age
--- Lab/django_forms/django_forms_app/views.py
+++ Lab/django_forms/django_forms_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import StudentForm
+from .forms import StudentForm, StudentModelForm
 
 # Create your views here.
 
@@ -7,8 +7,17 @@ def homePage(request):
     return render(request, 'index.html', {'form': StudentForm()})
 
 def processForm(request):
+    # Process the form data
     if request.POST:
         form = StudentForm(request.POST)
         if form.is_valid():
             print("DATA IS : ", form.cleaned_data)
+
+    # Process the ModelForm data
+    if request.POST:
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            print("Saving to the table.....")
+            form.save(commit=True)
+            print("Saved to the table*******")
     return render(request, 'index.html', {'form': StudentForm()})
