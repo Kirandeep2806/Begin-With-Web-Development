21ec1ce9de4fb0ff11ea022401f75660e13e0add
Kiran Deep <kirandeep102030@gmail.com>
2022-07-04 00:37:52 +0530

    Linked local files to the server using express

--- Lab/External Practice/expressServer.js
+++ Lab/External Practice/expressServer.js
@@ -1,8 +1,13 @@
-let express = require('express')
+const express = require('express')
+const path = require('path')
+
 const app = express()
 
+app.use(express.static(__dirname)) // adding this line will make the server look for static files in the current directory like linking local files to the server
+
 app.get('/', (req, res) => { // try writing /hell
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.sendFile(__dirname + '/jquery.html'); // it is same as res.sendFile(path.join(__dirname, 'jquery.html'))
 }
 )
 
